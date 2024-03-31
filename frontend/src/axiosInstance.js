355aa6efa2a4c474ad5124a031ed7708f3a204cc
ishanrev <ishanrev123@gmail.com>
2024-03-31 06:16:31 -0400

    Deployment changes

--- frontend/src/axiosInstance.js
+++ frontend/src/axiosInstance.js
@@ -1,7 +1,7 @@
 // const axiosLink = "http://localhost:3001/api"
 let axiosLink;
 if (process.env.NODE_ENV === "production") {
-    axiosLink = "https://structo-app.herokuapp.com/api"
+    axiosLink = "https://learninwave-c629a4eb000c.herokuapp.com/api"
 } else {
     axiosLink = "http://localhost:3001/api"
 }
